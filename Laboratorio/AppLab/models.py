2b6f4ac678133d76a149b5280ab08208741b8d56
Paola Girbal <paolagirbal@gmail.com>
2024-04-15 05:51:02 -0300

    Modelos listos

--- Laboratorio/AppLab/models.py
+++ Laboratorio/AppLab/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Practica(models.Model):
+
+    nombre = models.CharField(max_length=40)
+    asignatura = models.CharField(max_length=30)
+    fecha = models.DateField()
+
+class Docente(models.Model):
+
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    legajo = models.IntegerField()
+    email = models.EmailField()
+
+class Estudiante(models.Model):
+
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField()
